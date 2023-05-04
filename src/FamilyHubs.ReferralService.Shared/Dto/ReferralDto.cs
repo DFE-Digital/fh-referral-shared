a5ae8961a81cdb31eb1a0564c177ac4218b9ac0b
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-05-04 14:01:28 +0100

    Added Created and LastModified as required for the prototype

--- src/FamilyHubs.ReferralService.Shared/Dto/ReferralDto.cs
+++ src/FamilyHubs.ReferralService.Shared/Dto/ReferralDto.cs
@@ -9,6 +9,8 @@ public record ReferralDto : DtoBase<long>
     public required RecipientDto RecipientDto { get; set; }
     public required ReferrerDto ReferrerDto { get; set; }
     public required ReferralServiceDto ReferralServiceDto { get; set; }
+    public DateTime? Created { get; set; }
+    public DateTime? LastModified { get; set; }
     public ICollection<ReferralStatusDto> Status { get; set; } = new List<ReferralStatusDto>();
 
     public override int GetHashCode()
