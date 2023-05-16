fa82587bc6ce450952caac023811cd63ab1823ea
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-05-16 08:28:35 +0100

    Added MessageDto forGov Notify

---
+++ tests/FamilyHubs.ReferralService.Shared.Tests/MessageDtoComparerTests.cs
@@ -0,0 +1,23 @@
+﻿using FamilyHubs.ReferralService.Shared.Models;
+
+namespace FamilyHubs.ReferralService.Shared.Tests;
+
+public class MessageDtoComparerTests : PlainComparerTestBase<MessageDto, string>
+{
+    public MessageDtoComparerTests() : base(new MessageDto
+    {
+        RecipientEmail = "New",
+        TemplateId = "3f23e8ee-7692-4716-aaab-770b69965977"
+
+    }, new MessageDto
+    {
+        RecipientEmail = "New",
+        TemplateId = "3f23e8ee-7692-4716-aaab-770b69965977"
+
+
+    }, dto => dto.RecipientEmail)
+    {
+
+    }
+}
+
